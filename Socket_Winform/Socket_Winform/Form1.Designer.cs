542836e6bd7944d6fe0db7a30aa6b46d5e9b931d
EUBIN <41097199+danial1021@users.noreply.github.com>
2020-11-04 09:29:30 +0900

    basic ui

--- Socket_Winform/Socket_Winform/Form1.Designer.cs
+++ Socket_Winform/Socket_Winform/Form1.Designer.cs
@@ -28,13 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(181, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(533, 133);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "RED ON";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button2.Location = new System.Drawing.Point(1217, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(533, 133);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "RED OFF";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button3.Location = new System.Drawing.Point(1217, 310);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(533, 133);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "BLUE OFF";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Blue;
+            this.button4.Location = new System.Drawing.Point(181, 310);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(533, 133);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "BLUE ON";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button5.Location = new System.Drawing.Point(1217, 550);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(533, 133);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "GREEN OFF";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Lime;
+            this.button6.Location = new System.Drawing.Point(181, 550);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(533, 133);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "GREEN ON";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Silver;
+            this.button7.Location = new System.Drawing.Point(1217, 766);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(533, 133);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "ALL OFF";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Gray;
+            this.button8.Location = new System.Drawing.Point(181, 766);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(533, 133);
+            this.button8.TabIndex = 6;
+            this.button8.Text = "ALL ON";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(796, 438);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(343, 128);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "꿀따리";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1934, 1004);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label1;
     }
 }
 
